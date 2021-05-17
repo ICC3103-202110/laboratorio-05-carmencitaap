fac8a52721740ba25af76881ba95236db21039cd
carmencitaap <cmavendano@miuandes.cl>
2021-05-16 23:36:35 -0400

    inputs

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const {title} = require('./view')
 const {table} = require('./view')
+const {inputMoney} = require('./view')
 const {printTable} = require('console-table-printer')
 
 model = {
-    billAmount: '$'+0,
-    tipPercentage: 0+'%',
-    tip: '$'+0,
-    total: '$'+0,
+    billAmount: 0,
+    tipPercentage: 0,
+    tip: 0,
+    total: 0,
+    input: 0,
     input1: 0
 }
 
 async function app(update, title, table) {
+    console.clear()
     console.log(title())
     printTable(table(model))
+        
+    const {input,input1} = await inputMoney(model)
+    console.log(input,input1)
 }
 
 console.log(app(true, title,table))
--- view.js
+++ view.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const figlet = require('figlet')
+const inquirer = require('inquirer')
 
 function title(){
     return chalk.magenta(
@@ -19,12 +20,51 @@ function table(model) {
     const {tip} = model
     const {total} = model
     return [
-        {'Bill Amount': billAmount, 'Tip (%)': tipPercentage, 'Tip' : tip, 'Total' : total}
+        {'Bill Amount': '$'+billAmount, 'Tip (%)': tipPercentage+'%', 'Tip' : '$'+tip, 'Total' : '$'+total}
     ]
+}
+
+function inputMoney(model) {
+    const{input} = model
+    const{input1}
+    const message1 = 'Bill Amount?: '
+    const message2 = 'Tip? (%)'
+    return inquirer.prompt([
+        {
+            name: 'input',
+            type: 'number',
+            message: message1,
+            default: input,
+            validate: function(value){
+                if (value>=0){
+                    return true
+                }
+                else {
+                    return 'Enter a positive number'
+                }
+            }
+        },
+        {
+            name: 'input1',
+            type: 'number',
+            message: message2,
+            default: input1,
+            validate: function(value){
+                if (value>=0){
+                    return true
+                }
+                else {
+                    return 'Enter a positive number'
+                }
+            }
+        }
+    ])
+
 }
 
 
 module.exports = {
     title,
     table,
+    inputMoney
 }
